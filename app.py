8aef869f8d938f43b53f9cc6aa79cf18da8971aa
Jeremi Meneses <jmeneses@novopayment.com>
2018-09-23 11:23:52 -0500

    Adding a response

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # -*- coding: utf8 -*-
 import sys
 import os
+import requests
+import json
 from flask import Flask, request
 from flask import render_template
 from flask import send_file
@@ -22,7 +24,30 @@ def verify():
 def getMessage():
     data = request.get_json()
     log(data)
+    send_message(data['entry']['messaging']['sender']['id'], "HOLA!")
     return "OK", 200
+
+
+def send_message(recipient_id, message_text):
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
