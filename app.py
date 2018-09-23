f00ef555e7bd2bdc8b454e2f8af79ab2c6d56474
Jeremi Meneses <jmeneses@novopayment.com>
2018-09-23 13:23:08 -0500

    Fixing webhook

--- app.py
+++ app.py
@@ -27,10 +27,11 @@ def getMessage():
     if data["object"] == "page":
         user_id = data['entry'][0]['messaging'][0]['sender']['id']
         user = json.loads(get_user_by_id(user_id))
-        message = data['entry'][0]['messaging'][0]['message']
-        log(user["id"])
-        msg = "Gracias por su preferencia que tenga un buen dia 👋"
-        send_message(user["id"], message["text"])
+        if "message" in data['entry'][0]['messaging'][0]:
+            message = data['entry'][0]['messaging'][0]['message']
+            log(user["id"])
+            msg = "Gracias por su preferencia que tenga un buen dia 👋"
+            send_message(user["id"], message["text"])
 
     return "OK", 200
 
