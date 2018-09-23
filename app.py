ff3749e70b0cd6ea8589fd1fc311f1d1bc743e38
Jeremi Meneses <jmeneses@novopayment.com>
2018-09-23 18:46:06 -0500

    Adding terms and Conditions

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import sys
 import os
 import requests
 import json
+import pymongo
+import time
 from flask import Flask, request
 from flask import render_template
 from flask import send_file
+from pymongo.errors import DuplicateKeyError
 
 
 app = Flask(__name__)
@@ -24,14 +27,31 @@ def verify():
 def getMessage():
     data = request.get_json()
     log(data)
+    db = get_mongodb()
     if data["object"] == "page":
         if "message" in data['entry'][0]['messaging'][0]:
             user_id = data['entry'][0]['messaging'][0]['sender']['id']
-            user = json.loads(get_user_by_id(user_id))
-            message = data['entry'][0]['messaging'][0]['message']
-            log(user["id"])
-            msg = "Gracias por su preferencia que tenga un buen dia 👋"
-            send_message(user["id"], msg)
+            result = db.users.find({'id': user_id})
+            if result.count() is 0:
+                user = json.loads(get_user_by_id(user_id))
+                db.users.insert_one(user)
+            else:
+                for document in result:
+                    user = document
+
+            if "tyc" not in user:
+                message = data['entry'][0]['messaging'][0]['message']
+                # log(user)
+                msg = "Hola " + user["first_name"] + \
+                      " te ayudaré a realizar las consultas que necesites de tus tarjetas"
+                send_message(user["id"], msg)
+
+                send_termandc(user["id"])
+                time.sleep(2)
+                aceptTyC(user["id"])
+            else:
+                msg = "Hola " + user["first_name"]
+                send_message(user["id"], msg)
 
     return "OK", 200
 
@@ -43,7 +63,7 @@ def get_user_by_id(user_id):
     r = requests.get(url)
     if r.status_code != 200:
         log(r.status_code)
-        log(r.text)
+        # log(r.text)
         return r.status_code, r.text
     else:
         return r.text
@@ -71,9 +91,124 @@ def send_message(recipient_id, message_text):
         log(r.text)
 
 
+def send_termandc(recipient_id):
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        }, "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [
+                        {
+                            "title": "Novopayment",
+                            "subtitle": "Terminos y Condiciones del Servicio",
+                            "buttons": [
+                                {
+                                    "type": "web_url",
+                                    "url": "https://novopayment.com/privacy-policy/",
+                                    "title": "+ info"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    })
+    log(data)
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def aceptTyC(recipient_id):
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+              "recipient":{
+                "id":"1752570331535883"
+              },
+              "message":{
+                "text": "Solo tienes que hacer clic en \"Acepto\" para iniciar...",
+                "quick_replies":[
+                  {
+                    "content_type":"text",
+                    "title":"Acepto",
+                    "payload":"<POSTBACK_PAYLOAD>"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"No acepto",
+                    "payload":"<POSTBACK_PAYLOAD>"
+                  }
+                ]
+              }
+            })
+    log(data)
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def sendMenu():
+    menu = json.dumps({"persistent_menu":
+                {
+                  "locale": "default",
+                  "composer_input_disabled": True,
+                  "call_to_actions": [
+                    {
+                      "title":"My Account",
+                      "type":"nested",
+                      "call_to_actions": [
+                        {
+                          "title": "Pay Bill",
+                          "type": "postback",
+                          "payload": "PAYBILL_PAYLOAD"
+                        },
+                        {
+                          "type": "web_url",
+                          "title": "Latest News",
+                          "url": "https://www.messenger.com/",
+                          "webview_height_ratio": "full"
+                        }
+                      ]
+                    }
+                  ]
+                }
+    })
+    return menu
+
+
+
+
 def log(message):  # simple wrapper for logging to stdout on heroku
     print(str(message))
     sys.stdout.flush()
+
+
+def get_mongodb():
+    try:
+        db = None
+        log(os.environ["MONGO"])
+        _dev = pymongo.MongoClient(os.environ["MONGO"])
+        db = _dev[os.environ["SCHEMA"]]
+    except Exception as e:
+        log("Error: " + str(e.args))
+    return db
 
 
 if __name__ == '__main__':
