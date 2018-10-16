c2fc77f2f144619e5a4531f29f626bc9cc18a219
Jeremi Meneses <jmeneses@novopayment.com>
2018-10-16 16:31:43 -0500

    - Validating SMS confirmation code. (Fixing response for SMS confirmation)

--- app.py
+++ app.py
@@ -232,8 +232,8 @@ def save_user_information(user, message, db):
         confirmation = only_numerics(message)
         if confirmation["rc"] == 0:
             response = {"rc": 0, "msg": "Process OK"}
-            confirmationTime = user["date-confirmation"] - datetime.now()
-            log(confirmationTime)
+            confirmationTime = datetime.now() - user["date-confirmation"]
+            log(confirmationTime.seconds)
             if confirmationTime.seconds > 180:
                 send_message(user["id"], "El código ya expiro. ")
                 send_message(user["id"], "para continuar necesito enviarte un codigo de activación.")
