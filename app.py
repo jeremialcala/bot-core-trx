455932887de8829527cec4e6f5077fd5f20a2998
Jeremi Meneses <jmeneses@novopayment.com>
2018-10-18 22:01:02 -0500

    - Find user by Name(showing multiples match)

--- app.py
+++ app.py
@@ -225,8 +225,12 @@ def get_user_by_name(name, operation, db):
             buttons["type"] = "postback"
             buttons["payload"] = operation + "|" + friend["id"]
             elements["buttons"].append(buttons)
+        if result.count() > 1:
+            payload["template_type"] = "list"
+            payload["top_element_style"] = "compact"
         payload["elements"].append(elements)
         attachment["payload"] = payload
+
         return "OK", 200, attachment
 
 
