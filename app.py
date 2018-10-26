41d65fd4b9e39a24ea1dec93e0e1f4221fa0bf0a
Jeremi Meneses <jmeneses@novopayment.com>
2018-10-26 12:13:02 -0500

    - MoneySend (Fixing typo on accountId)

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def get_message():
                     if "TRX_" in messaging["message"]["quick_reply"]["payload"]:
                         action = messaging["message"]["quick_reply"]["payload"].split("_")
                         transaction = db.transactions.find_one({"_id": ObjectId(action[3])})
-                        account = db.accountPool.find_one({"_id": ObjectId(user["account_id"])})
+                        account = db.accountPool.find_one({"_id": ObjectId(user["accountId"])})
                         friend = user.find_one({"id": transaction["recipient"]})
                         if action[1] is "N":
                             payload = {"template_type": "receipt", "recipient_name": "Eduardo Cold",
